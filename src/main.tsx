9fd4e4a4280e3a635c0e3ca7b7b79d6f7edef93f
hayazaki <toru.hayazaki@gmail.com>
2023-09-06 07:07:10 +0900

    prettierの設定

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        aaxx
+      </a>
+      <span>sss</span>
+    </div>
+  </React.StrictMode>
+);
