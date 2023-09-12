e4c3763f6f203ca54805ef1529c814ca511463f2
hayazaki <toru.hayazaki@gmail.com>
2023-09-12 23:50:19 +0900

    ButtonComponentのstorybookを追加

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,12 +2,17 @@ import "./Button.scss";
 
 type ButtonProps = {
   text: string;
+  backgroundColor?: string;
   onClick: () => void;
 };
 
-const ButtonComponent = ({ text, onClick }: ButtonProps) => {
+const ButtonComponent = ({
+  text,
+  backgroundColor = "white",
+  onClick,
+}: ButtonProps) => {
   return (
-    <div className="button" onClick={onClick}>
+    <div className="button" style={{ backgroundColor }} onClick={onClick}>
       {text}
     </div>
   );
