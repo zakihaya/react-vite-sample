f3ed59801e2ea630881ad384f5a5eb6f4485a96d
hayazaki <toru.hayazaki@gmail.com>
2023-09-22 01:09:57 +0900

    testを修正

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,24 +1,37 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import { expect, test } from "vitest";
+import { BrowserRouter } from "react-router-dom";
 import App from "@/App";
 
 test("renders currentTeamId", () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 
   const teamIdTitleLabel = screen.getByText(/^currentTeamId: \d{13}$/);
   expect(teamIdTitleLabel).toBeInTheDocument();
 });
 
 test("renders Add Button", () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 
   const addButton = screen.getByRole("button", { name: "Add" });
   expect(addButton).toBeInTheDocument();
 });
 
 test("renders Refresh TeamId Button", () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 
   const refreshTeamIdButton = screen.getByRole("button", {
     name: "Refresh TeamId",
